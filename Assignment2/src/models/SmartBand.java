8b8678303a29cbd734f6a367b423b2aa365dff62
Kobe-Bryant-fuhuo <jbyx555@163.com>
2024-05-18 13:02:24 +0800

    1

--- Assignment2/src/models/SmartBand.java
+++ Assignment2/src/models/SmartBand.java
@@ -1,4 +1,46 @@
 package models;
 
-public class SmartBand {
+import utils.Utilities;
+
+public  class SmartBand extends WearableDevice{
+
+
+    public void setHeartRateMonitor(boolean heartRateMonitor) {
+        this.heartRateMonitor = heartRateMonitor;
+    }
+
+    private boolean heartRateMonitor;
+    public SmartBand(String modelName,double price,Manufacturer manufacturer,String id,String size,String material){
+        super(modelName, price, manufacturer, id,size,material);
+    }
+
+    @Override
+    public double getInsurancePremium() {
+        return getPrice()*0.07;
+    }
+
+    @Override
+    public String connectToInternet() {
+        return "Connects to the internet via Companion App";
+    }
+    @Override
+    public String toString() {
+        if(heartRateMonitor=true){
+            return "smartband{" + "InsurancePremium" + getInsurancePremium() + "connectToInternet" + connectToInternet() + "Includes Heart Rate Monitor" +"WearableDevice{" +
+                    "size='" + getSize() + '\'' +
+                    ", material='" + getMaterial() + '\'' +
+                    '}';
+        }else {
+            return "smartband{" + "InsurancePremium" + getInsurancePremium() + "connectToInternet" + connectToInternet() + "not Includes Heart Rate Monitor" + "WearableDevice{" +
+                    "size='" + getSize()+ '\'' +
+                    ", material='" + getMaterial() + '\'' +
+                    '}';
+        }
+
+    }
+    public boolean isHeartRateMonitor(){
+        return true;
+
+    }
 }
+
--- Assignment2/src/models/SmartWatch.java
+++ Assignment2/src/models/SmartWatch.java
@@ -1,4 +1,49 @@
 package models;
+import utils.Utilities;
+public  class  SmartWatch extends WearableDevice{
 
-public class SmartWatch {
-}
+    public SmartWatch(String modelName, double price, Manufacturer manufacturer, String id, String size, String material) {
+        super(modelName, price, manufacturer, id, size, material);
+    }
+
+    public String getDisplaytype() {
+        return displaytype;
+    }
+
+    public void setDisplaytype(String displaytype) {
+        if (displaytype=="LCD") {
+            this.displaytype = displaytype;
+        }else if(displaytype=="LED") {
+            this.displaytype = displaytype;
+        }
+        if (displaytype=="TFT") {
+            this.displaytype = displaytype;
+        }else if(displaytype=="AMOLED") {
+            this.displaytype = displaytype;
+        }
+    }
+
+    private String displaytype="LCD";
+
+
+    @Override
+    public double getInsurancePremium() {
+        return  getPrice()*0.06;
+    }
+
+    @Override
+    public String connectToInternet() {
+        return "Connects to the internet via bluetooth";
+    }
+    @Override
+    public String toString() {
+
+            return "smartwatch{" + "InsurancePremium" + getInsurancePremium() + "connectToInternet" + connectToInternet() + "Includes Heart Rate Monitor" +"WearableDevice{" +
+                    "size='" + getSize() + '\'' +
+                    ", material='" + getMaterial() + '\'' +
+                    '}'+"displaytype"+getDisplaytype();
+
+        }
+
+    }
+
