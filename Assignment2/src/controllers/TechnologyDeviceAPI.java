b19be9bd27bdb7f8b826fe184b119ac0ed33a323
MAChi-NUIST <2302500712@qq.com>
2024-05-19 11:11:25 +0800

    add change

--- Assignment2/src/controllers/TechnologyDeviceAPI.java
+++ Assignment2/src/controllers/TechnologyDeviceAPI.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import models.*;
 
 import utils.ISerializer;
@@ -13,43 +15,39 @@ import java.util.*;
 
 
 //TODO - ensure that this class implements iSerializer
-public class TechnologyDeviceAPI {
-    public TechnologyDeviceAPI(File file) {
-    }
+public class TechnologyDeviceAPI implements ISerializer{
 
-    public void load() {
-    }
 
-    public int numberOfTechnologyDevices() {
-    }
 
-    public void addTechnologyDevice(Tablet newTab) {
-    }
 
-    public String listAllTechnologyDevices() {
-    }
 
-    public String listAllTechnologyAbovePrice(double v) {
-    }
 
-    public Object getTechnologyByIndex(int i) {
-    }
 
-    public void sortByPriceDescending() {
-    }
     //TODO - create 2 fields
+    private List<Technology> technologyList=new ArrayList<>();
+private File file;
 
     //TODO - create constructor
+public TechnologyDeviceAPI(File file) {
+    this.file=file;
+}
    //TODO - CRUD Methods
-
+//Create method
+    public boolean addTechnologyDevice(Technology technology){}
 
 
 
     //TODO - Number methods
-
+    public int numberOfTechnologyDevices() {
+    }
 
     // TODO Read/list methods
-
+    public Object getTechnologyByIndex(int i) {
+    }
+    public String listAllTechnologyDevices() {
+    }
+    public String listAllTechnologyAbovePrice(double v) {
+    }
 
 
     //the following is isValidId can be updated
@@ -65,20 +63,43 @@ public class TechnologyDeviceAPI {
 
     //TODO get Technology methods
 
-   //TODO - delete methods
+    public List<Technology> getTechnologyList() {
+        return technologyList;
+    }
+
+    //TODO - delete methods
 
 
 
     //TODO - sort methods
-
+    public void sortByPriceDescending() {
+    }
     //TODO Top 5 methods
-
+public List<Technology> topFiveMostExpenxiveTablet(){}
 
 
 
 
     // TODO Persistence methods
-
+    @Override
+    public String fileName() {
+        return String.valueOf(file);
+    }
+    public void save() throws Exception{
+        var xstream = new XStream(new DomDriver());
+        ObjectOutputStream os = xstream.createObjectOutputStream(new FileWriter(file));
+        os.writeObject(technologyList);
+        os.close();
+    }
+    public void load() throws Exception{
+        Class<?>[] classes = new Class[]{ Technology.class};
+        XStream xstream = new XStream(new DomDriver());
+        XStream.setupDefaultSecurity(xstream);
+        xstream.allowTypes(classes);
+        ObjectInputStream in = xstream.createObjectInputStream(new FileReader(file));
+        technologyList = (List<Technology>) in.readObject();
+        in.close();
+    }
 
 
 
